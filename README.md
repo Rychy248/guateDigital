2c3017c80beff1f1c648f806015187a8da80cc9a
Rychy <jorgeajrhh@gmail.com>
2023-04-19 18:48:19 -0600

    FUNCION REALIZADA

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # guateDigital
+
+Repositirio de almacen para los detalles que cubren la entrevista/Prueba
--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import time
+import pandas as pd
+
 
 def precioProducto(code):
     
+    # Opciones de navegación
+    options =  webdriver.ChromeOptions()
+    options.add_argument('--start-maximized')
+    options.add_argument('--disable-extensions')
+
+    driver_path = './chromedriver.exe'
+
+    driver = webdriver.Chrome(driver_path, chrome_options=options)
+
+    # Iniciarla en la pantalla 2
+    driver.set_window_position(2000, 0)
+    driver.maximize_window()
+    time.sleep(1)
+
+    # Inicializamos el navegador
     url = f"https://guatemaladigital.com/Producto/{code}"
-    driver = webdriver.Firefox()
-    
-    driver.get("http://www.python.org")
-    
-    assert "Python" in driver.title
-    elem = driver.find_element(By.NAME, "q")
-    elem.clear()
-    elem.send_keys("pycon")
-    elem.send_keys(Keys.RETURN)
-    assert "No results found." not in driver.page_source
-    driver.close()
+    driver.get(url)
+
+    price = driver.find_element( By.XPATH,  '//*[@id="columnaA"]/div[3]/h4[1]/span/span')
+    price = price.text
+
+    driver.quit()
 
     # "precioQuetzales":209,
     print("El precio es: ",price,"")
-    print(soup.get("h4"))
 
 precioProducto(5041295)
